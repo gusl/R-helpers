c5701e8ccf55d56a93ec11855b37074e0a72ba91
Gustavo Lacerda <gusg@optimizelife.com>
2013-01-04 18:27:41 -0300

    third

--- grid.R
+++
@@ -1,7 +0,0 @@
-## NOTE: makeGrid(0,1,10) is the same seq(0,1,len=11)
-makeGrid <- function(left,right,m) { d <- m/(right-left); ((left*d):(right*d))/d }
-linspace <- makeGrid
-
-makeExpoGrid <- function(left, right, m){
-  exp(makeGrid(log(left), log(right),m))
-}
